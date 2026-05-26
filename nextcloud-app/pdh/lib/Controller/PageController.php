dfcb220e512f2b264e2f02e3b56112badd934abe
Michael Strobl <37264057+strobobaer@users.noreply.github.com>
2026-05-26 16:24:04 +0200

    feat: make embedded PDH URL configurable

--- nextcloud-app/pdh/lib/Controller/PageController.php
+++ nextcloud-app/pdh/lib/Controller/PageController.php
@@ -5,14 +5,17 @@ use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IConfig;
 use OCP\IRequest;
 
 class PageController extends Controller {
     private ?string $userId;
+    private IConfig $config;
 
-    public function __construct(string $appName, IRequest $request, ?string $userId) {
+    public function __construct(string $appName, IRequest $request, ?string $userId, IConfig $config) {
         parent::__construct($appName, $request);
         $this->userId = $userId;
+        $this->config = $config;
     }
 
     #[NoAdminRequired]
@@ -20,6 +23,7 @@ class PageController extends Controller {
     public function index(): TemplateResponse {
         return new TemplateResponse('pdh', 'main', [
             'userId' => $this->userId ?? '',
+            'pdhPublicUrl' => $this->config->getAppValue('pdh', 'public_url', 'https://pdh.strobl-home.net'),
         ]);
     }
 }
