4ff6ffad75971298bb1a5f497dcafe1edb9103fc
Michael Strobl <37264057+strobobaer@users.noreply.github.com>
2026-05-26 15:51:52 +0200

    feat: add Nextcloud PDH app routes

---
+++ nextcloud-app/pdh/appinfo/routes.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'routes' => [
+        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+        ['name' => 'api#status', 'url' => '/api/status', 'verb' => 'GET'],
+    ],
+];
