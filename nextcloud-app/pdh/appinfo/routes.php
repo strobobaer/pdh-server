bebbccd76e37f1861bdd64a31f35f9aaeee51a53
Michael Strobl <37264057+strobobaer@users.noreply.github.com>
2026-05-27 06:36:35 +0200

    feat: add Nextcloud PDH SSO launch route

--- nextcloud-app/pdh/appinfo/routes.php
+++ nextcloud-app/pdh/appinfo/routes.php
@@ -3,5 +3,6 @@ return [
     'routes' => [
         ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
         ['name' => 'api#status', 'url' => '/api/status', 'verb' => 'GET'],
+        ['name' => 'sso#launch', 'url' => '/auth/launch', 'verb' => 'GET'],
     ],
 ];
